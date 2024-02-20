9868d9eb5da5c8efbbb249e2481e83b3a4ed26c8
mkim925 <mkim925@gatech.edu>
2024-02-20 14:43:56 -0500

    GanttChart updated

--- ganttChart.py
+++ ganttChart.py
@@ -2,9 +2,18 @@ import pandas as pd
 import streamlit as st
 from st_pages import add_page_title
 from st_aggrid import AgGrid
+import xlsxwriter
+from io import BytesIO
 
 st.title("📝 Gantt Chart")
 
 df = pd.read_excel("Proposal/GanttChart.xlsx", usecols = 'A:F')
 AgGrid(df)
 
+with open("Proposal/GanttChart.xlsx", "rb") as f:
+    b = f.read()
+
+st.download_button(label="Download the full Gantt Chart Spreadsheet",
+                    data=b,
+                    file_name="GanttChart.xlsx",
+                    mime='application/octet-stream')
